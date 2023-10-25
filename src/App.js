c999799691b476d4dc3511964016963c081a781f
Ranjith677 <107233964+Ranjith677@users.noreply.github.com>
2023-10-25 12:32:49 +0530

    1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import { bootstarp } from "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './navbar';
+import Footer from './footer';
+
+function App() {
+  return (
+    <>
+    <Navbar></Navbar>
+    <Footer></Footer>
+    </>
+  
+  );
+}
+
+export default App;
